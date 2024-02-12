fe2728996da0ff1e77e62aec42756ec0a6db7d18
valrichter <valenrichter11@gmail.com>
2024-02-12 20:14:55 -0300

    refator tests

--- backend/db/tests/user_test.go
+++ backend/db/tests/user_test.go
@@ -10,28 +10,47 @@ import (
 	"github.com/valrichter/Ualapp/util"
 )
 
-func TestCreateUser(t *testing.T) {
-	passwordLength := util.RandomInt(6, 20)
-	hashedPassword := util.RandomPassword(passwordLength)
+// createRandomUser creates a random user for tests
+func createRandomUser(t *testing.T) db.User {
+	password := util.RandomPassword(util.RandomInt(6, 20))
+	hashedPassword, err := util.HashPassword(password)
+	require.NoError(t, err)
 
 	arg := db.CreateUserParams{
 		Email:          util.RandomEmail(),
 		HashedPassword: hashedPassword,
 	}
 
-	user1, err := testQuery.CreateUser(context.Background(), arg)
+	user, err := testQuery.CreateUser(context.Background(), arg)
 
 	require.NoError(t, err)
-	require.NotEmpty(t, user1)
+	require.NotEmpty(t, user)
 
-	require.Equal(t, arg.Email, user1.Email)
-	require.Equal(t, arg.HashedPassword, user1.HashedPassword)
-	require.NotZero(t, user1.CreatedAt)
-	require.WithinDuration(t, user1.CreatedAt.Time, time.Now(), 2*time.Second)
-	require.WithinDuration(t, user1.UpdatedAt.Time, time.Now(), 2*time.Second)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.NotZero(t, user.CreatedAt)
+	require.WithinDuration(t, user.CreatedAt.Time, time.Now(), 2*time.Second)
+	require.WithinDuration(t, user.UpdatedAt.Time, time.Now(), 2*time.Second)
+
+	return user
+}
+
+// TestCreateUser tests the CreateUser function
+func TestCreateUser(t *testing.T) {
+
+	user1 := createRandomUser(t)
+
+	arg := db.CreateUserParams{
+		Email:          user1.Email,
+		HashedPassword: user1.HashedPassword,
+	}
 
 	user2, err := testQuery.CreateUser(context.Background(), arg)
 	require.Error(t, err)
 	require.Empty(t, user2)
 }
 
+// TestUpdateUser
+func TestUpdateUser(t *testing.T) {
+
+}
