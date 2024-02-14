669a6944de6a13c120ebd7803e7c1f4e3f00755e
valrichter <valenrichter11@gmail.com>
2024-02-14 20:01:42 -0300

    add PostgreSQLStore

---
+++ backend/db/sqlc/store.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store interface {
+	Querier
+}
+
+// SQLStore provides all functions to execute SQL queries & transactions
+type PostgreSQLStore struct {
+	connPool *pgxpool.Pool
+	*Queries
+}
+
+// Creates a new Store
+func NewStore(connPool *pgxpool.Pool) Store {
+	return &PostgreSQLStore{
+		connPool: connPool,
+		Queries:  New(connPool),
+	}
+}
