b4ecd66b8577c2cf65cb0c8499d75ce8a99de1f8
valrichter <valenrichter11@gmail.com>
2024-02-15 12:43:56 -0300

    add server struct

--- backend/api/server.go
+++ backend/api/server.go
@@ -4,7 +4,17 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/valrichter/Ualapp/db/sqlc"
 )
+
+// Server serves HTTP requests for our banking service
+// Contains a store to access the database
+// Contains a gin engine to serve HTTP requests
+
+type Server struct {
+	store db.Store
+	gin   *gin.Engine
+}
 
 // NewServer creates a new HTTP server and setup routing
 func NewGinServer(port int) {
