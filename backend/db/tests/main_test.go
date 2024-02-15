2fcfd68b4c54cb8d0b88e58d3465f80f7b4033ca
valrichter <valenrichter11@gmail.com>
2024-02-15 13:26:58 -0300

    add app.env

--- backend/db/tests/main_test.go
+++ backend/db/tests/main_test.go
@@ -8,16 +8,20 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	db "github.com/valrichter/Ualapp/db/sqlc"
+	"github.com/valrichter/Ualapp/util"
 )
 
 var testQuery db.Store
 
-// TODO: put this in a config file
 // TestMain sets up the database connection everytime the tests are run
 func TestMain(m *testing.M) {
-	const dbURL string = "postgresql://root:secret@localhost:5432/ualapp?sslmode=disable"
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
 
-	connPoll, err := pgxpool.New(context.Background(), dbURL)
+	// TODO: change this to use in test database
+	connPoll, err := pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database: ", err)
 	}
---
+++ backend/util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Congif configures application level variables
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+// LoadConfig reads configuration from file or environment variables and returns a capy of Config struct
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return
+	}
+	return config, nil
+}
