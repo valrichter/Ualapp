c3704fb8abe0ae1484bf9af20c9a55e42f239456
valrichter <valenrichter11@gmail.com>
2024-02-17 17:24:36 -0300

    add check on database

--- backend/api/auth.go
+++ backend/api/auth.go
@@ -4,19 +4,50 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/valrichter/Ualapp/util"
 )
 
+// Auth struct to handle authentication
 type Auth struct {
 	server *Server
 }
 
-func (a Auth) router(server *Server) {
-	a.server = server
+// Routing for authentication
+func (auth Auth) router(server *Server) {
+	auth.server = server
 
 	serverGroup := server.router.Group("/auth")
-	serverGroup.POST("/login", a.login)
+	serverGroup.POST("/login", auth.login)
 }
 
-func (a Auth) login(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Login Route"})
+// Login function for authentication
+func (auth Auth) login(ctx *gin.Context) {
+	user := new(userRequest)
+	if err := ctx.ShouldBindJSON(user); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	dbUser, err := auth.server.store.GetUserByEmail(ctx, user.Email)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": "Incorrect mail",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	err = util.CheckPassword(user.Password, dbUser.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Incorrect password",
+		})
+		return
+	}
+
 }
