c05c1c83830cd0cf3d256bdd7938669656f433c8
valrichter <valenrichter11@gmail.com>
2024-02-28 17:16:24 -0300

    add transaction func

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -20,4 +23,25 @@ func NewPostgreSQLStore(connPool *pgxpool.Pool) Store {
 		connPool: connPool,
 		Queries:  New(connPool),
 	}
+}
+
+// Executes a function within a database transaction
+func (store *PostgreSQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
+
+	// Initialize a transaction
+	tx, err := store.connPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit(ctx)
 }
