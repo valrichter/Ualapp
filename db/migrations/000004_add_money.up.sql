8c534c672aa409afdabf0895ed7819dbd3c029f7
valrichter <valenrichter11@gmail.com>
2024-04-10 19:38:13 -0300

    update DB

--- db/migrations/000004_add_money.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE
-    money_records (
-        id SERIAL PRIMARY KEY,
-        user_id uuid NOT NULL REFERENCES users ON DELETE CASCADE,
-        reference VARCHAR(50) UNIQUE NOT NULL,
-        status VARCHAR(50) NOT NULL,
-        amount bigint NOT NULL
-    );
